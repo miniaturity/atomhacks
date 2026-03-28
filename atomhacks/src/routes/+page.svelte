9baabc5a3fac3f17c0d9551b698e19aa318fdb7c
Miniaturity <miniaturity2.o@gmail.com>
2026-03-28 09:19:04 -0400

    init commit

---
+++ atomhacks/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+<div class="page">
+
+</div>
+<style lang="scss">
+  :global(:root) {
+    --bg: #070D0D;
+      
+  }
+
+  :global(body) {
+    margin: 0; padding: 0;
+  }
+  .page {
+
+    width: 100vw; height: 100vh;
+  }
+</style>        
